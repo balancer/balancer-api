c191f1c7833fb5669d901e0d847b6e52ec868009
Tim Robinson <tim@timjrobinson.com>
2021-11-29 18:30:00 +1000

    Use SOR from latest npm version instead of John's branch

--- src/worker.ts
+++ src/worker.ts
@@ -4,36 +4,21 @@
  */
 
 require("dotenv").config();
-import { SOR } from "@balancer-labs/sor2";
 import BigNumber from "bignumber.js";
 import debug from "debug";
 import { JsonRpcProvider } from '@ethersproject/providers';
-import { Network, Order } from "./types";
+import { Network } from "./types";
+import { fetchPoolsFromChain } from "./sor";
 import { updatePools } from "./dynamodb";
 
 const log = debug("balancer");
 
-const { MAX_POOLS, INFURA_PROJECT_ID } = process.env;
+const { INFURA_PROJECT_ID } = process.env;
 
 const nodeUrl = `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`;
-const poolsSource = 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-v2';
-const chainId = Network.MAINNET;
-const gasPrice = new BigNumber('30000000000');
-let maxPools: number = 4;
-if (MAX_POOLS)
-  maxPools = Number(MAX_POOLS);
-
 
 log(`Using Infura endpoint ${nodeUrl}`);
 const provider: any = new JsonRpcProvider(nodeUrl);
-
-const sor = new SOR(
-  provider,
-  gasPrice,
-  maxPools,
-  chainId,
-  poolsSource
-);
 
 let lastBlockNumber = 0;
 
@@ -43,10 +28,9 @@ async function doWork() {
 
   if(currentBlockNo !== lastBlockNumber){
     log(`New block ${currentBlockNo} found!`);
-    log(`Fetching Pools`);
-    await sor.fetchPools(true);
-    log(`Saving pools to database`);
-    const pools = sor.onChainBalanceCache.pools;
+    log(`Fetching pools from chain`)
+    const pools = await fetchPoolsFromChain();
+    log(`Saving ${pools.length} pools to database`);
     await updatePools(pools);
     log(`Saved pools`);
     lastBlockNumber = currentBlockNo;

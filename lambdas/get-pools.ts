63160eadb6902d59e5f4add72aa533a3d9c174d6
Tim Robinson <timjrobinson@users.noreply.github.com>
2022-02-01 23:54:43 +1000

    Add CORS headers to pools lambdas (#7)

--- lambdas/get-pools.ts
+++ lambdas/get-pools.ts
@@ -6,10 +6,16 @@ export const handler = async (event: any = {}): Promise<any> => {
     return { statusCode: 400, body: `Error: You are missing the chainId` };
   }
 
+  const corsHeaders = {
+    "Access-Control-Allow-Headers" : "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,GET"
+  }
+
   try {
     const pools = await getPools(chainId);
-    return { statusCode: 200, body: JSON.stringify(pools) };
+    return { statusCode: 200, headers: corsHeaders, body: JSON.stringify(pools) };
   } catch (dbError) {
-    return { statusCode: 500, body: JSON.stringify(dbError) };
+    return { statusCode: 500, headers: corsHeaders, body: JSON.stringify(dbError) };
   }
 };

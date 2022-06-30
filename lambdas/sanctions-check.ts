cd326620a0ccd533f4b708c6bfdf04bcf13b6bcf
Tim Robinson <tim@timjrobinson.com>
2022-06-30 11:22:33 +0900

    Only log when an error occurs

--- lambdas/sanctions-check.ts
+++ lambdas/sanctions-check.ts
@@ -14,7 +14,6 @@ export const handler = async (event: any = {}): Promise<any> => {
     return { statusCode: 400, body: `Error: You are missing the address in the body` };
   }
 
-  console.log("Posting addres: ", address, " to endpoint: ", SANCTIONS_ENDPOINT);
   try {
     const response = await fetch(SANCTIONS_ENDPOINT, {
       method: 'POST',
@@ -29,11 +28,9 @@ export const handler = async (event: any = {}): Promise<any> => {
     });
 
     const result = await response.json();
-    console.log("Got result: ", result);
-
     return { statusCode: 200, body: JSON.stringify(result) };
   } catch (e) {
-    console.log(`Received error performing sanctions check: ${e}`)
+    console.log(`Received error performing sanctions check on address ${address}: ${e}`)
     return { statusCode: 500, body: '' };
   }
 };

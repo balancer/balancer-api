452daf7c55af16c35332a37c0e6c51fccbf3b0f5
Tim Robinson <tim@timjrobinson.com>
2022-09-20 00:29:12 +0200

    Add secondary index to local DynamoDB. Make local server use real lambda functions

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
+import { DynamoDB } from "aws-sdk";
 import { Schema, Network } from "./types";
 
 export const COINGECKO_BASEURL = 'https://api.coingecko.com/api/v3';
@@ -8,45 +9,62 @@ export const MAX_BATCH_WRITE_SIZE = 25;
 export const MAX_DYNAMODB_PRECISION = 38;
 
 export const TEST_NETWORKS: number[] = [
-    Network.GOERLI,
-    Network.KOVAN
+  Network.GOERLI,
+  Network.KOVAN
 ];
 
 export const PRODUCTION_NETWORKS: number[] = Object.values(Network).filter((networkId) => {
   return TEST_NETWORKS.indexOf(networkId) === -1;
 })
 
-export const POOLS_TABLE_SCHEMA = {
-  TableName : "pools",
-  KeySchema: [       
-      { AttributeName: "id", KeyType: "HASH"},
-      { AttributeName: "chainId", KeyType: "RANGE"},
+export const POOLS_TABLE_SCHEMA: DynamoDB.Types.CreateTableInput = {
+  TableName: "pools",
+  KeySchema: [
+    { AttributeName: "id", KeyType: "HASH" },
+    { AttributeName: "chainId", KeyType: "RANGE" },
   ],
-  AttributeDefinitions: [       
-      { AttributeName: "id", AttributeType: "S" },
-      { AttributeName: "chainId", AttributeType: "N" },
+  AttributeDefinitions: [
+    { AttributeName: "id", AttributeType: "S" },
+    { AttributeName: "chainId", AttributeType: "N" },
+    { AttributeName: "totalLiquidity", AttributeType: "S" },
   ],
-  ProvisionedThroughput: {       
-      ReadCapacityUnits: 10, 
-      WriteCapacityUnits: 10
-  }
-}
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10,
+  },
+  GlobalSecondaryIndexes: [
+    {
+      IndexName: "byTotalLiquidity",
+      KeySchema: [
+        { AttributeName: "id", KeyType: "HASH" },
+        { AttributeName: "totalLiquidity", KeyType: "RANGE" },
+      ],
+      Projection: {
+        ProjectionType: "ALL",
+      },
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5,
+      },
+    },
+  ],
+};
 
 export const TOKENS_TABLE_SCHEMA = {
-  TableName : "tokens",
-  KeySchema: [       
-      { AttributeName: "address", KeyType: "HASH"},
-      { AttributeName: "chainId", KeyType: "RANGE"},
+  TableName: "tokens",
+  KeySchema: [
+    { AttributeName: "address", KeyType: "HASH" },
+    { AttributeName: "chainId", KeyType: "RANGE" },
   ],
-  AttributeDefinitions: [       
-      { AttributeName: "address", AttributeType: "S" },
-      { AttributeName: "chainId", AttributeType: "N" },
+  AttributeDefinitions: [
+    { AttributeName: "address", AttributeType: "S" },
+    { AttributeName: "chainId", AttributeType: "N" },
   ],
-  ProvisionedThroughput: {       
-      ReadCapacityUnits: 10, 
-      WriteCapacityUnits: 10
-  }
-}
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10,
+  },
+};
 
 /** 
  * Used for marshalling / unmarshalling into DynamoDB
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import express from "express";
 import { getSorSwap } from "./sor";
 import { getPool, getPools, getToken, getTokens } from "./data-providers/dynamodb";
 import { isValidChainId, localAWSConfig } from "./utils";
+import { handler as getPoolsHandler } from "./lambdas/get-pools";
+import { handler as getPoolHandler } from "./lambdas/get-pool";
+import { handler as sorHandler } from "./lambdas/run-sor";
+import { handler as getTokensHandler } from './lambdas/get-tokens';
 
 const log = debug("balancer");
 
@@ -15,53 +19,33 @@ AWS.config.update(localAWSConfig);
 const port = PORT || 8090;
 const app = express();
 
-app.get("/pools/:chainId", async (req, res, next) => {
-  log("Geting pools");
-  try {
-    const chainId = Number(req.params['chainId']);
-    if (!isValidChainId(chainId)) return res.sendStatus(404);
-    const pools = await getPools(chainId);
-    res.json(pools);
-  } catch (error) {
-    log(`Error: ${error.message}`);
-    return next(error);
-  }
+app.get("/pools/:chainId", async (req, res) => {
+  const result = await getPoolsHandler({
+      pathParameters: req.params
+  });
+  res.status(result.statusCode).send(result.body);
 });
 
 app.get("/pools/:chainId/:id", async (req, res) => {
-  const chainId = Number(req.params['chainId']);
-  const poolId = req.params['id'];
-  log(`Retrieving pool of id ${poolId}`);
-  const pool = await getPool(chainId, poolId);
-  if (pool) {
-    return res.json(pool)
-  } else {
-    return res.sendStatus(404);
-  }
+  const result = await getPoolHandler({
+    pathParameters: req.params
+  });
+  res.status(result.statusCode).send(result.body);
 });
 
-app.post("/sor/:chainId", express.json(), async (req, res, next) => {
-  try{
-    const chainId = Number(req.params['chainId']);
-    const swapInfo = await getSorSwap(chainId, req.body);
-    res.json(swapInfo);
-  } catch(error){
-    log(`Error: ${error.message}`);
-    return next(error);
-  }
+app.post("/sor/:chainId", express.json(), async (req, res) => {
+  const result = await sorHandler({
+    pathParameters: req.params,
+    body: req.body
+  });
+  res.status(result.statusCode).send(result.body);
 });
 
-app.get("/tokens/:chainId", async (req, res, next) => {
-  log("Getting tokens");
-  try {
-    const chainId = Number(req.params['chainId']);
-    if (!isValidChainId(chainId)) return res.sendStatus(404);
-    const tokens = await getTokens(chainId);
-    res.json(tokens);
-  } catch (error) {
-    log(`Error: ${error.message}`);
-    return next(error);
-  }
+app.get("/tokens/:chainId", async (req, res) => {
+  const result = await getTokensHandler({
+    pathParameters: req.params
+  });
+  res.status(result.statusCode).send(result.body);
 });
 
 app.get("/tokens/:chainId/:id", async (req, res) => {

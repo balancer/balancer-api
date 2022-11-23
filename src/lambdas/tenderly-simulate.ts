252af3c31cf1bc121e5dd1a86976b97003653171
sssmi <simeon.kerkola@gmail.com>
2022-11-23 17:15:36 +0200

    Fix data formats

--- src/lambdas/tenderly-simulate.ts
+++ src/lambdas/tenderly-simulate.ts
@@ -17,29 +17,27 @@ function formatResponse(statusCode, body) {
   };
 }
 
-export const handler = async ({ body, ...event }: any = {}): Promise<any> => {
+export const handler = async ({ body }: any = {}): Promise<any> => {
   if (!body) {
     return formatResponse(
       400,
       'Error: invalid request, you are missing the request body'
     );
   }
-  console.log({ body: body, event });
+
   try {
     const res = await fetch(TENDERLY_ENDPOINT, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'X-Access-Key': Buffer.from(
-          `${TENDERLY_ACCESS_KEY}:${TENDERLY_ACCESS_KEY}`
-        ).toString('base64'),
+        'X-Access-Key': TENDERLY_ACCESS_KEY,
       },
-      body: typeof body == 'object' ? body : JSON.parse(body),
+      body: typeof body == 'string' ? body : JSON.stringify(body),
     });
     console.log({ res });
     const result = await res.json();
     console.log({ result });
-    return formatResponse(200, result);
+    return formatResponse(200, JSON.stringify(result));
   } catch (e) {
     console.log(`Error when trying to simulate: ${e}`);
     return formatResponse(500, 'Unable to perform simulation');

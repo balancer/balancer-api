55d6f1a5f27b458adbfb11eaa3af179cf18dc150
Tim Robinson <tim@timjrobinson.com>
2023-02-09 17:13:51 +1000

    Add script to benchmark SOR with different minLiquidities

---
+++ src/scripts/benchmark-sor.ts
@@ -0,0 +1,86 @@
+require('dotenv').config();
+import apiBenchmark from 'api-benchmark';
+import { SorRequest } from '../types';
+import { TOKENS, Network, TokenWithSlot } from '../constants';
+import { parseFixed } from '@ethersproject/bignumber';
+import util from 'util';
+
+const ENDPOINT_URL = process.env.ENDPOINT_URL || 'https://api.balancer.fi';
+const NETWORK = 1;
+
+const { DAI, BAL, USDC, WETH, auraBal } = TOKENS[Network.MAINNET];
+
+const GAS_PRICE = parseFixed('50', 9).toString();
+
+
+function createSorPayload(sellToken: TokenWithSlot, buyToken: TokenWithSlot): SorRequest {
+  return {
+    sellToken: sellToken.address,
+    buyToken: buyToken.address,
+    orderKind: 'sell',
+    amount: parseFixed('100', sellToken.decimals).toString(),
+    gasPrice: GAS_PRICE,
+  }
+}
+
+
+const sorPayloads: SorRequest[] = [
+  createSorPayload(BAL, DAI),
+  createSorPayload(USDC, DAI),
+  createSorPayload(WETH, BAL),
+  createSorPayload(WETH, auraBal),
+];
+
+
+function createRoute(minLiquidity: number, payload: SorRequest) {
+  return {
+    method: 'post',
+    route: `?useDb=1&minLiquidity=${minLiquidity}`,
+    data: payload,
+  };
+}
+
+function createSubgraphRoutes() {
+  return Object.fromEntries(sorPayloads.map((payload, idx) => {
+    const key = `subgraph-${idx}`;
+    return [key, {
+      method: 'post',
+      route: '',
+      data: payload
+    }];
+  }));
+}
+
+
+function createRoutes(minLiquidity: number) {
+  return Object.fromEntries(sorPayloads.map((payload, idx) => {
+    const key = `${minLiquidity}-${idx}`;
+    return [key, createRoute(minLiquidity, payload)];
+  }));
+}
+
+
+const service = {
+  api: `${ENDPOINT_URL}/sor/${NETWORK}/`,
+};
+
+const routes = { 
+  ...createSubgraphRoutes(),
+  // ...createRoutes(0.001),
+  // ...createRoutes(0.01),
+  // ...createRoutes(0.1),
+  // ...createRoutes(1),
+  // ...createRoutes(10),
+  // ...createRoutes(100),
+  ...createRoutes(1000),
+};
+
+const options = {
+  debug: true,
+  stopOnError: true,
+  minSamples: 5
+};
+
+apiBenchmark.compare(service, routes, options, (err, results) => {
+  console.log(util.inspect(results, false, null));
+});

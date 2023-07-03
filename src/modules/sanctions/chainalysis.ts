96721a1e01790bddf821d51c409a904a947c8178
Tim Robinson <tim@timjrobinson.com>
2023-07-03 18:41:06 +1000

    Remove old check

--- src/modules/sanctions/chainalysis.ts
+++ src/modules/sanctions/chainalysis.ts
@@ -7,8 +7,6 @@ import { getRpcUrl } from '@/modules/network';
 const sanctionsContractAddress = '0x40C57923924B5c5c5455c48D93317139ADDaC8fb'
 
 export async function isSanctioned(address: string): Promise<boolean> {
-    if (!this.sanctionsContractAddress) return false;
-
     const rpcUrl = getRpcUrl(1);
     this.provider = new JsonRpcProvider(rpcUrl);
     const sanctionsContract = new Contract(sanctionsContractAddress, abi, this.provider);

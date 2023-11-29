be98a11fcc482b898f31b0901970161318976905
Gareth Fuller <gareth.a.f@gmail.com>
2023-11-29 20:39:04 +0000

    chore: Set batch limit to 10

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -1,5 +1,5 @@
 export const COINGECKO_BASEURL = 'https://api.coingecko.com/api/v3';
-export const COINGECKO_MAX_TOKENS_PER_PAGE = 100;
+export const COINGECKO_MAX_TOKENS_PER_PAGE = 10;
 export const COINGECKO_MAX_TPS = 10;
 
 export const MAX_BATCH_WRITE_SIZE = 5;

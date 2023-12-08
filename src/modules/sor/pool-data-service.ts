6fef126e1c2d0c5f0d21da6e83b7f5db7c01109d
gmbronco <83549293+gmbronco@users.noreply.github.com>
2023-12-08 06:51:44 +0100

    fix: SOR should ignore some pools

--- src/modules/sor/pool-data-service.ts
+++ src/modules/sor/pool-data-service.ts
@@ -3,6 +3,7 @@ import { PoolDataService } from '@balancer-labs/sor';
 import { SubgraphPoolBase } from '@balancer-labs/sdk';
 import { queryPools } from '@/modules/dynamodb';
 import { Pool, convertPoolToSubgraphPoolBase } from '@/modules/pools';
+import poolsToIgnore from './pools-to-ignore';
 
 const log = debug('balancer:pool-data-service');
 
@@ -32,7 +33,10 @@ export class DatabasePoolDataService implements PoolDataService {
   public async getPools(): Promise<SubgraphPoolBase[]> {
     log(`Retrieving pools for chain ${this.chainId} from the database`);
 
-    const pools: Pool[] = await queryPools(this.filterParams);
+    const allPools: Pool[] = await queryPools(this.filterParams);
+    const pools = allPools.filter(
+      pool => !poolsToIgnore.includes(pool.id.toLowerCase())
+    );
 
     log(`Retrieved ${pools.length} pools`);
     const subgraphPools = pools.map(pool =>

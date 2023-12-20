3353bfec35f989c9f625779d9b7ffb231c16cb78
Gareth Fuller <gareth.a.f@gmail.com>
2023-12-20 19:49:31 +0000

    chore: Add coingecko api key

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -1,4 +1,4 @@
-export const COINGECKO_BASEURL = 'https://api.coingecko.com/api/v3';
+export const COINGECKO_BASEURL = 'https://pro-api.coingecko.com/api/v3';
 export const COINGECKO_MAX_TOKENS_PER_PAGE = 10;
 export const COINGECKO_MAX_TPS = 10;
 
